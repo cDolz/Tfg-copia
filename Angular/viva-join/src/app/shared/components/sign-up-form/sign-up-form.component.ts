7ea29b0155e12cb47eecf31917d53046caa8c07f
cDolz <carlosdolzm@gmail.com>
2024-04-04 20:20:05 +0200

    master: form correction, TODO service

--- Angular/viva-join/src/app/shared/components/sign-up-form/sign-up-form.component.ts
+++ Angular/viva-join/src/app/shared/components/sign-up-form/sign-up-form.component.ts
@@ -1,11 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-shared-sign-up-form',
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.scss'
 })
-export class SignUpFormComponent {  
+export class SignUpFormComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
 
   // cojo el año actual
   currentYear: number = new Date().getFullYear();
@@ -22,5 +26,16 @@ export class SignUpFormComponent {
 
   // array del año actual -120 años para el select
   years: number[] = Array.from({ length: 120 }, (_, i) => this.currentYear - i);
-  
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+      name: new FormControl(),
+      surname: new FormControl(),      
+    })
+  }
+
 }
--- Angular/viva-join/src/app/shared/shared.module.ts
+++ Angular/viva-join/src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.comp
 import { SwiperModule } from 'swiper/angular';
 import { EventInfoComponent } from './components/event-info/event-info.component';
 import { EventImgCarouselComponent } from './components/event-img-carousel/event-img-carousel.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [LoginFormComponent, SignUpFormComponent, EventScrollComponent, EventInfoComponent, EventImgCarouselComponent],
     exports: [LoginFormComponent, SignUpFormComponent, EventScrollComponent, EventInfoComponent, EventImgCarouselComponent, SwiperModule],
-    imports: [CommonModule, SwiperModule]
+    imports: [CommonModule, SwiperModule, ReactiveFormsModule]
 })
 export class SharedModule { }
